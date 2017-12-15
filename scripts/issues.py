feab92d1bc8c834a8cf96babf1c4f902e2144541
RyanBaten <rbb.baten@gmail.com>
2017-12-14 21:57:18 -0700

    issues update for counts

--- scripts/issues.py
+++ scripts/issues.py
@@ -14,6 +14,21 @@ args = parser.parse_args()
 results_succ = defaultdict(lambda : defaultdict(float))
 results_fail = defaultdict(lambda : defaultdict(float))
 
+# Results gathering for types files
+type_column_names = ['month','total_count','succ_count','fail_count','succ_opened','fail_opened','succ_closed','fail_closed','succ_reopened','fail_reopened']
+type_data_columns = ['succ_count','fail_count','succ_opened','fail_opened','succ_closed','fail_closed','succ_reopened','fail_reopened']
+
+for infile in args.types:
+    data = pandas.read_csv(str(infile), header=None, delim_whitespace=True, names=type_column_names)
+    months = data.month.unique() 
+    for month in months:
+        month_data = data[data.month == month]
+        for column in type_data_columns:
+            if 'succ' in column:
+                results_succ[column[5:]][0] = month_data[column].iloc[0]
+            elif 'fail' in column:
+                results_fail[column[5:]][0] = month_data[column].iloc[0]
+
 # Results gathering for count files
 column_names = ['count','repo','month','comments','body','success']
 data_columns = ['count','comments','body']
@@ -29,41 +44,60 @@ for infile in args.files:
         results_fail[month]['nobs'] = month_data_fail.shape[0]
         for column in data_columns:
             # Get data 
-            results_succ[month][column + '_mean'] = month_data_succ[column].mean()
-            results_succ[month][column + '_stddev'] = month_data_succ[column].std()
-            results_fail[month][column + '_mean'] = month_data_fail[column].mean()
-            results_fail[month][column + '_stddev'] = month_data_fail[column].std()
+            results_succ[month][str(column) + '_mean'] = month_data_succ[column].mean()
+            results_succ[month][str(column) + '_stddev'] = month_data_succ[column].std()
+            results_fail[month][str(column) + '_mean'] = month_data_fail[column].mean()
+            results_fail[month][str(column) + '_stddev'] = month_data_fail[column].std()
 
+keys = list(results_succ.keys())
+keys.remove("opened")
+keys.remove("closed")
+keys.remove("reopened")
+keys.remove("count")
+keys.sort(key=int)
 
-# Results gathering for types files
-type_column_names = ['month','total_count','succ_count','fail_count','succ_opened','fail_opened','succ_closed','fail_closed','succ_reopened','fail_reopened']
-type_data_columns = ['succ_count','fail_count','succ_opened','fail_opened','succ_closed','fail_closed','succ_reopened','fail_reopened']
-
-for infile in args.types:
-    data = pandas.read_csv(str(infile), header=None, delim_whitespace=True, names=type_column_names)
-    months = data.month.unique() 
-    for month in months:
-        month_data = data[data.month == month]
-        for column in type_data_columns:
-            if 'succ' in column:
-                results_succ[column[5:]] = month_data[column].iloc[0]
-
-for month in sorted(results_succ.keys()):
-    print("Month: " + str(month))
+for month in keys:
+    print("\nMonth: " + str(month))
     for key in data_columns:
-        try:
-            m1 = results_succ[month][key + "_mean"]
-            m2 = results_fail[month][key + "_mean"]
-            stddev1 = results_succ[month][key + "_stddev"]
-            stddev2 = results_fail[month][key + "_stddev"]
-            nobs1 = results_succ[month]["nobs"]
-            nobs2 = results_fail[month]["nobs"]
-            print("Success Mean: " + str(m1))
-            print("Success StdDev: " + str(stddev1))
-            print("Success Nobs: " + str(nobs1))
-            print("Failure Mean: " + str(m2))
-            print("Failure StdDev: " + str(stddev2))
-            print("Failure Nobs: " + str(nobs2))
-            print("T-Test: " + str(scipy.stats.ttest_ind_from_stats(m1,stddev1,nobs1,m2,stddev1,nobs2)))
-        except:
-            continue
+        print(key + ":")
+        m1 = results_succ[month][key + "_mean"]
+        m2 = results_fail[month][key + "_mean"]
+        stddev1 = results_succ[month][key + "_stddev"]
+        stddev2 = results_fail[month][key + "_stddev"]
+        nobs1 = results_succ[month]["nobs"]
+        nobs2 = results_fail[month]["nobs"]
+        print("Success Mean: " + str(m1))
+        print("Success StdDev: " + str(stddev1))
+        print("Success Nobs: " + str(nobs1))
+        print("Failure Mean: " + str(m2))
+        print("Failure StdDev: " + str(stddev2))
+        print("Failure Nobs: " + str(nobs2))
+        print("T-Test: " + str(scipy.stats.ttest_ind_from_stats(m1,stddev1,nobs1,m2,stddev1,nobs2)))
+        m1 /= results_succ["count"][0]
+        m2 /= results_fail["count"][0]
+        stddev1 /= results_succ["count"][0]
+        stddev2 /= results_fail["count"][0]
+        print("Success Scaled Mean: " + str(m1))
+        print("Success Scaled StdDev: " + str(stddev1))
+        print("Success Scaled Nobs: " + str(nobs1))
+        print("Failure Scaled Mean: " + str(m2))
+        print("Failure Scaled StdDev: " + str(stddev2))
+        print("Failure Scaled Nobs: " + str(nobs2))
+        print("Scaled T-Test: " + str(scipy.stats.ttest_ind_from_stats(m1,stddev1,nobs1,m2,stddev1,nobs2)))
+        print()
+
+print("Total Succ " + str(results_succ["count"][0]))
+print("Opened Succ " + str(results_succ["opened"][0]))
+print("Closed Succ " + str(results_succ["closed"][0]))
+print("Reopened Succ " + str(results_succ["reopened"][0]))
+print("Total Fail " + str(results_fail["count"][0]))
+print("Opened Fail " + str(results_fail["opened"][0]))
+print("Closed Fail " + str(results_fail["closed"][0]))
+print("Reopened Fail " + str(results_fail["reopened"][0]))
+
+print("Opened Succ/Total Succ " + str(results_succ["opened"][0]/results_succ["count"][0]))
+print("Closed Succ/Total Succ " + str(results_succ["closed"][0]/results_succ["count"][0]))
+print("Reopened Succ/Total Succ " + str(results_succ["reopened"][0]/results_succ["count"][0]))
+print("Opened Fail/Total Fail " + str(results_fail["opened"][0]/results_fail["count"][0]))
+print("Closed Fail/Total Fail " + str(results_fail["closed"][0]/results_fail["count"][0]))
+print("Reopened Fail/Total Fail " + str(results_fail["reopened"][0]/results_fail["count"][0]))
